067d29262ee0aae0d41c3dde628e57f04b741504
Eder Ramirez <documentador1@telecomo.com.mx>
2018-05-23 17:29:03 -0500

    Lifecycle methods

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -29,6 +29,16 @@ var IndecisionApp = function (_React$Component) {
     }
 
     _createClass(IndecisionApp, [{
+        key: 'componentDidMount',
+        value: function componentDidMount() {
+            console.log('fetching data!');
+        }
+    }, {
+        key: 'componentDidUpdate',
+        value: function componentDidUpdate(prevProps, prevState) {
+            console.log('saving data!');
+        }
+    }, {
         key: 'handleDeleteOptions',
         value: function handleDeleteOptions() {
             this.setState({ options: [] });
