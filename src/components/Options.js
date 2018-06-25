cea955658e0386ea19af848653ec4c3aa39bc984
Eder Ramirez <documentador1@telecomo.com.mx>
2018-06-25 16:48:15 -0500

    Styling option item

--- src/components/Options.js
+++ src/components/Options.js
@@ -3,15 +3,19 @@ import Option from './Option'
 
 const Options = (props) => (
     <div>
-        <button 
-            className="button button--link"
-            onClick={props.handleDeleteOptions}>RemoveAll</button>
-        {props.options.length === 0 && <p>Please add an option to get started!</p>}
+        <div className="widget-header">
+            <h3 className="widget-header__title">Your Options</h3>
+            <button 
+                className="button button--link"
+                onClick={props.handleDeleteOptions}>RemoveAll</button>
+        </div>
+        {props.options.length === 0 && <p className="widget__message">Please add an option to get started!</p>}
         { 
             props.options.map((option,index) => (
                 <Option 
                     option={option} 
                     key={index}
+                    count={index +1}
                     handleDeleteOption={props.handleDeleteOption}/>
             )) 
         }
