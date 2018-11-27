8e105544c29f8028da6f8e9b2e8d7ce20cdc38fb
Eder Ramírez Hernández <eder.ramirez87@gmail.com>
2018-11-27 05:46:23 -0600

    Rendering individual expenses

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ import getVisibleExpenses from './selectors/expenses'
 
 const store = configureStore();
 
-store.dispatch(addExpense({description:'Water bill'}))
+store.dispatch(addExpense({description:'Water bill', amount: 4500}))
 store.dispatch(addExpense({description:'Gas bill'}))
 store.dispatch(setTextFilter('Water'))
 
