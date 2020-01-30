8054ae3d3662723f675ea4b6d36a58890434dcbc
Eder Ramirez <eder.ramirez87@gmail.com>
2020-01-30 16:44:57 -0600

    Testing async redux actions part II

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -2,6 +2,7 @@ import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import { startAddExpense, addExpense, editExpense, removeExpense }  from '../../actions/expenses'
 import expenses from '../fixtures/expenses';
+import database from '../../firebase/firebase'
 
 const createMockStore = configureMockStore([thunk]);
 
@@ -33,7 +34,7 @@ test('should setup ad expense action object with provided values', () => {
     })
 })
 
-test('Should add expense to database and store', () => {
+test('Should add expense to database and store', (done) => {
   const store = createMockStore({});
   const expenseData = {
     description: 'Mouse',
@@ -44,13 +45,57 @@ test('Should add expense to database and store', () => {
 
   store.dispatch(startAddExpense(expenseData))
   .then(() => {
-    expect(9).toBe(1)
+    const actions = store.getActions();
+    expect(actions[0]).toEqual({
+      type: 'ADD_EXPENSE',
+      expense: {
+        id: expect.any(String),
+        ...expenseData
+      }
+    });
+
+    return database.ref(`expenses/${actions[0].expense.id}`).once('value')
+  })
+  .then(snapshot => {
+    expect(snapshot.val()).toEqual(expenseData);
+    done();
   });
 });
 
-test('should add expense with defaults to database and store', () => {
+test('Should add expense with defaults to database and store', (done) => {
+  const store = createMockStore({});
+  const expenseData = {
+    description: '',
+    amount: 0,
+    note: '',
+    createdAt: 0
+  }
 
-})
+  store.dispatch(startAddExpense({}))
+  .then(() => {
+    const actions = store.getActions();
+    expect(actions[0]).toEqual({
+      type: 'ADD_EXPENSE',
+      expense: {
+        id: expect.any(String),
+        ...expenseData
+      }
+    });
+
+    return database.ref(`expenses/${actions[0].expense.id}`).once('value')
+  })
+  .then(snapshot => {
+    expect(snapshot.val()).toEqual(expenseData);
+    done();
+  }).catch(err => {
+    console.log("TCL: err", err)
+    done();
+  });
+});
+
+/* test('should add expense with defaults to database and store', () => {
+
+}) */
 
 /* test('should setup ad expense action object with default values', () => {
     const action = addExpense()
--- src/tests/components/ExpensesSummary.test.js
+++ src/tests/components/ExpensesSummary.test.js
@@ -3,13 +3,13 @@ import { ExpensesSummary } from '../../components/ExpensesSummary';
 import { shallow } from 'enzyme'
 import expenses from '../fixtures/expenses'
 
-test('Should render correctly without expenses', () => {
+/* test('Should render correctly without expenses', () => {
   const wrapper = shallow(<ExpensesSummary  count={0} total={0}/>)
   const text = wrapper.find('h1').text()
   
   expect(wrapper).toMatchSnapshot(wrapper)
   expect(text).toBe('Viewing 0 expenses totalling $0.00')
-})
+}) */
 
 test('Should render correctly with expenses', () => {
   const wrapper = shallow(<ExpensesSummary count={3} total={10000}/>)
